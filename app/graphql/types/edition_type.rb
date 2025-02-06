b37431166e48db8303e10610bda5445ea0e5fcc2
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2025-02-06 11:03:04 +0000

    Add all columns to edition type
    Note this is not really enough for the frontend, as some fields need processing (e.g. details->'body' might need converting to HTML). There's also more stuff here than the frontends actually need, but hey ho.

--- app/graphql/types/edition_type.rb
+++ app/graphql/types/edition_type.rb
@@ -1,14 +1,36 @@
 module Types
   class EditionType < Types::BaseObject
+    field :id, Integer, null: false
     field :title, String, null: false
+    field :public_updated_at, String, null: false
+    field :publishing_app, String, null: false
+    field :rendering_app, String, null: false
+    field :update_type, String, null: true
+    field :phase, String, null: true
+    field :analytics_identifier, String, null: true
+    field :created_at, String, null: false
+    field :updated_at, String, null: false
+    field :document_type, String, null: false
+    field :schema_name, String, null: false
+    field :first_published_at, String, null: true
+    field :last_edited_at, String, null: true
+    field :state, String, null: false
+    field :user_facing_version, String, null: true
+    field :base_path, String, null: true
+    field :content_store, String, null: false
+    field :document_id, Integer, null: false
+    field :description, String, null: true
+    field :publishing_request_id, String, null: true
+    field :major_published_at, String, null: true
+    field :published_at, String, null: true
+    field :publishing_api_first_published_at, String, null: true
+    field :publishing_api_last_edited_at, String, null: true
+    field :details, GraphQL::Types::JSON, null: false
+    field :routes, GraphQL::Types::JSON, null: true
+    field :redirects, GraphQL::Types::JSON, null: true
+    field :last_edited_by_editor_id, String, null: true
+    field :content_id, String, null: false
+
     field :links, LinksType
-
-    def title
-      object[:title]
-    end
-
-    def links
-      object[:links]
-    end
   end
 end
