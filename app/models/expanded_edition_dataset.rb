9b29bc5a21d7d244134da65839c80b868ac84720
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2025-02-09 02:38:46 +0000

    Refactor column selections

--- app/models/expanded_edition_dataset.rb
+++ app/models/expanded_edition_dataset.rb
@@ -19,51 +19,50 @@ class ExpandedEditionDataset
     link_type_ds = db[paths_from_json_sql, :$link_type_paths]
     reverse_link_type_ds = db[paths_from_json_sql, :$reverse_link_type_paths]
 
-    def columns(type, root: false)
-      [
-        Sequel[type].as(:type),
-        (root ? Sequel.pg_array([], "text") : Sequel[:edition_links][:path].pg_array.push(:link_type)).as(:path),
-        (root ? Sequel.pg_array([ Sequel[:editions][:id] ], "int") : Sequel[:edition_links][:id_path].pg_array.push(Sequel[:editions][:id])).as(:id_path),
-        Sequel[:documents][:content_id].as(:content_id),
-        Sequel[:documents][:locale].as(:locale),
-        Sequel[:editions][:id].as(:edition_id),
-        *(
-          root ?
-            # Include all columns for the root edition
-            PathTreeHelpers::ALL_EDITION_COLUMNS.map { |col| Sequel[:editions][col] } :
-            # Filter columns for other editions
-            PathTreeHelpers::ALL_EDITION_COLUMNS.map do |col|
-              Sequel.case({ Sequel.lit("(columns->'#{col}')::boolean") => Sequel[:editions][col] }, nil).as(col)
-            end
-        ),
-      ]
-    end
+    root_selections = [
+      Sequel.pg_array([], "text").as(:path),
+      Sequel.pg_array([ Sequel[:editions][:id] ], "int").as(:id_path),
+      Sequel[:documents][:content_id].as(:content_id),
+      Sequel[:documents][:locale].as(:locale),
+      Sequel[:editions][:id].as(:edition_id),
+      *PathTreeHelpers::ALL_EDITION_COLUMNS.map { Sequel[:editions][it] }
+    ]
+    child_selections = [
+      Sequel[:edition_links][:path].pg_array.push(:link_type).as(:path),
+      Sequel[:edition_links][:id_path].pg_array.push(Sequel[:editions][:id]).as(:id_path),
+      Sequel[:documents][:content_id].as(:content_id),
+      Sequel[:documents][:locale].as(:locale),
+      Sequel[:editions][:id].as(:edition_id),
+      *PathTreeHelpers::ALL_EDITION_COLUMNS.map do |col|
+        Sequel.case({ Sequel.lit("(columns->'#{col}')::boolean") => Sequel[:editions][col] }, nil).as(col)
+      end
+    ]
 
     root_edition = db[:editions]
                      .join(:documents, id: :document_id)
                      .where(state: "published", locale: "en", base_path: :$base_path)
-                     .select(*columns("root", root: true))
+                     .select(Sequel["0 - root"].as(:type), *root_selections)
 
     edition_links_and_paths = db[:edition_links].join(:link_type_paths, path: :path)
     forward_editions = edition_links_and_paths
                          .join(:links, edition_id: Sequel[:edition_links][:edition_id], link_type: Sequel[:link_type_paths][:next])
                          .join(:documents, content_id: :target_content_id, locale: "en")
                          .join(:editions, document_id: :id, state: "published")
-                         .select(*columns("forward edition"))
+                         .select(Sequel["1 - forward edition"].as(:type), *child_selections)
 
     forward_link_sets = edition_links_and_paths
                           .join(:link_sets, content_id: Sequel[:edition_links][:content_id])
                           .join(:links, link_set_id: :id, link_type: Sequel[:link_type_paths][:next])
                           .join(:documents, content_id: :target_content_id, locale: "en")
                           .join(:editions, document_id: :id, state: "published")
-                          .select(*columns("forward link set"))
+                          .select(Sequel["2 - forward link set"].as(:type), *child_selections)
 
     reverse_editions = db[:edition_links]
                          .join(:reverse_link_type_paths, path: :path)
                          .join(:links, target_content_id: Sequel[:edition_links][:content_id], link_type: Sequel[:reverse_link_type_paths][:next])
                          .join(:editions, id: :edition_id, state: "published")
                          .join(:documents, id: :document_id, locale: "en")
-                         .select(*columns("reverse edition"))
+                         .select(Sequel["3 - reverse edition"].as(:type), *child_selections)
 
     reverse_link_sets = db[:edition_links]
                           .join(:reverse_link_type_paths, path: :path)
@@ -71,7 +70,7 @@ class ExpandedEditionDataset
                           .join(:link_sets, id: :link_set_id)
                           .join(:documents, content_id: :content_id, locale: "en")
                           .join(:editions, document_id: :id, state: "published")
-                          .select(*columns("reverse link set"))
+                          .select(Sequel["4 - reverse link set"].as(:type), *child_selections)
 
     recursive_term =
       forward_editions
