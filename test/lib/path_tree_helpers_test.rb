4ae14cef8eee5611f5554fd364497e5b13132d6f
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2025-02-10 16:54:28 +0000

    Fix test_extract_paths

--- test/lib/path_tree_helpers_test.rb
+++ test/lib/path_tree_helpers_test.rb
@@ -2,31 +2,52 @@
 require "ostruct"
 require "minitest/autorun"
 require_relative "../../lib/path_tree_helpers"
+require_relative "../test_helper"
 
 class PathTreeHelpersTest < Minitest::Test
+  Selection = Data.define(:name, :arguments, :selections)
   def test_extract_paths
     lookahead = Minitest::Mock.new
     lookahead.expect :selections, [
-      OpenStruct.new(name: :links, selections: [
-        OpenStruct.new(name: :person, selections: [
-          OpenStruct.new(name: :links, selections: [
-            OpenStruct.new(name: :role, selections: [])
-          ])
+      Selection.new(name: :links, arguments: {}, selections: [
+        Selection.new(name: :links_of_type, arguments: { name: "person" }, selections: [
+          Selection.new(name: :base_path, arguments: {}, selections: []),
+          Selection.new(name: :title, arguments: {}, selections: []),
+          Selection.new(name: :links, arguments: {}, selections: [
+            Selection.new(name: :links_of_type, arguments: { name: "person", reverse: true }, selections: [
+              Selection.new(name: :base_path, arguments: {}, selections: []),
+              Selection.new(name: :title, arguments: {}, selections: []),
+              Selection.new(name: :links, arguments: {}, selections: [
+                Selection.new(name: :links_of_type, arguments: { name: "role" }, selections: [
+                  Selection.new(name: :base_path, arguments: {}, selections: []),
+                  Selection.new(name: :title, arguments: {}, selections: []),
+                ]),
+              ]),
+            ]),
+          ]),
         ]),
-        OpenStruct.new(name: :organisation, selections: [
-          OpenStruct.new(name: :links, selections: [
-            OpenStruct.new(name: :employees, selections: [])
-          ])
-        ])
-      ])
+        Selection.new(name: :links_of_type, arguments: { name: "organisation" }, selections: [
+          Selection.new(name: :base_path, arguments: {}, selections: []),
+          Selection.new(name: :title, arguments: {}, selections: []),
+        ]),
+      ]),
     ]
 
     expected_paths = [
       [],
-      %i[person],
-      %i[person role],
-      %i[organisation],
-      %i[organisation employees]
+      [{ name: "person", columns: { base_path: true, title: true } }],
+      [
+        { name: "person", columns: { base_path: true, title: true } },
+        { name: "person", reverse: true, columns: { base_path: true, title: true } },
+      ],
+      [
+        { name: "person", columns: { base_path: true, title: true } },
+        { name: "person", reverse: true, columns: { base_path: true, title: true } },
+        { name: "role", columns: { base_path: true, title: true } },
+      ],
+      [
+        { name: "organisation", columns: { base_path: true, title: true } },
+      ],
     ]
 
     assert_equal expected_paths, PathTreeHelpers.extract_paths(lookahead)
@@ -38,29 +59,32 @@ class PathTreeHelpersTest < Minitest::Test
       { id_path: [1, 2], path: %w[link_type], content_id: "link1" },
       { id_path: [1, 2, 3], path: %w[link_type person], content_id: "person1" },
       { id_path: [1, 2, 4], path: %w[link_type person], content_id: "person2" },
-      { id_path: [1, 2, 5], path: %w[link_type role], content_id: "role1" }
+      { id_path: [1, 2, 5], path: %w[link_type role], content_id: "role1" },
     ]
 
     expected_tree = {
-      id_path: [1], path: [], content_id: "root",
+      id_path: [1],
+      path: [],
+      content_id: "root",
       links: {
         "link_type" => [
           {
-            id_path: [1, 2], path: ["link_type"], content_id: "link1",
+            id_path: [1, 2],
+            path: %w[link_type],
+            content_id: "link1",
             links: {
               "person" => [
-                { id_path: [1, 2, 3], path: ["link_type", "person"], content_id: "person1" },
-                { id_path: [1, 2, 4], path: ["link_type", "person"], content_id: "person2" }
+                { id_path: [1, 2, 3], path: %w[link_type person], content_id: "person1" },
+                { id_path: [1, 2, 4], path: %w[link_type person], content_id: "person2" },
               ],
               "role" => [
-                { id_path: [1, 2, 5], path: ["link_type", "role"], content_id: "role1" }
-              ]
-            }
-          }
-        ]
-      }
+                { id_path: [1, 2, 5], path: %w[link_type role], content_id: "role1" },
+              ],
+            },
+          },
+        ],
+      },
     }
-
 
     assert_equal expected_tree, PathTreeHelpers.nest_results(rows)
   end
