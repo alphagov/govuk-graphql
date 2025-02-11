3e73f711853ca1a10b5222e97b3117a13fcbf593
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2025-02-11 17:28:00 +0000

    Add a GraphiQL user interface

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
   include GDS::SSO::ControllerMethods
   before_action :authenticate_user!
 end
--- config/application.rb
+++ config/application.rb
@@ -39,7 +39,7 @@ module GovukGraphql
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.api_only = true
+    config.api_only = false
 
     config.sequel.after_connect = proc do
       Sequel.extension :pg_array_ops
