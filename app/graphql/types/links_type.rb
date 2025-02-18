78f73cd71ed73ef8b23bdb4a99dba31fde1097ee
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2025-02-18 10:31:40 +0000

    Implement available_translations

--- app/graphql/types/links_type.rb
+++ app/graphql/types/links_type.rb
@@ -1,8 +1,15 @@
 module Types
   class LinksType < Types::BaseObject
+    field :available_translations, [EditionType], null: true
     field :links_of_type, [EditionType] do
       argument :type, String, required: true
       argument :reverse, Boolean, default_value: false
+    end
+
+    def available_translations
+      Sequel::Model.db[:editions]
+                   .join(:documents, id: :document_id, content_id: object[:content_id])
+                   .where(state: object[:include_drafts] ? %w[published draft] : %w[published])
     end
 
     # rubocop:disable Lint/UnusedMethodArgument
