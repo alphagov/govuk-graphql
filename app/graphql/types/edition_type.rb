658c2aaf73ec3f7e56b2e84fad5e3b88608ce4a2
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2025-02-19 16:27:38 +0000

    Use datetimes instead of strings

--- app/graphql/types/edition_type.rb
+++ app/graphql/types/edition_type.rb
@@ -2,29 +2,29 @@ module Types
   class EditionType < Types::BaseObject
     field :id, Integer, null: false
     field :title, String, null: false
-    field :public_updated_at, String, null: false
+    field :public_updated_at, GraphQL::Types::ISO8601DateTime, null: false
     field :publishing_app, String, null: false
     field :rendering_app, String, null: false
     field :update_type, String, null: true
     field :phase, String, null: true
     field :analytics_identifier, String, null: true
-    field :created_at, String, null: false
-    field :updated_at, String, null: false
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
     field :document_type, String, null: false
     field :schema_name, String, null: false
-    field :first_published_at, String, null: true
-    field :last_edited_at, String, null: true
+    field :first_published_at, GraphQL::Types::ISO8601DateTime, null: true
+    field :last_edited_at, GraphQL::Types::ISO8601DateTime, null: true
     field :state, String, null: false
-    field :user_facing_version, String, null: true
+    field :user_facing_version, Integer, null: true
     field :base_path, String, null: true
     field :content_store, String, null: false
     field :document_id, Integer, null: false
     field :description, String, null: true
     field :publishing_request_id, String, null: true
-    field :major_published_at, String, null: true
-    field :published_at, String, null: true
-    field :publishing_api_first_published_at, String, null: true
-    field :publishing_api_last_edited_at, String, null: true
+    field :major_published_at, GraphQL::Types::ISO8601DateTime, null: true
+    field :published_at, GraphQL::Types::ISO8601DateTime, null: true
+    field :publishing_api_first_published_at, GraphQL::Types::ISO8601DateTime, null: true
+    field :publishing_api_last_edited_at, GraphQL::Types::ISO8601DateTime, null: true
     field :publishing_scheduled_at, GraphQL::Types::ISO8601DateTime, null: true
     field :scheduled_publishing_delay_seconds, Int, null: true
 
