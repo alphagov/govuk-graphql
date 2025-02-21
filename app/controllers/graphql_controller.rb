43496bc0a9cf24cdbb8bf79540190c8e071bcb11
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2025-02-21 16:36:48 +0000

    Add benchmarks to graphql output

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "benchmark"
+
 class GraphqlController < ApplicationController
   protect_from_forgery with: :null_session, if: -> { request.headers["Authorization"].present? }
   protect_from_forgery with: :exception, unless: -> { request.headers["Authorization"].present? }
@@ -14,13 +16,18 @@ class GraphqlController < ApplicationController
       # Query context goes here, for example:
       # current_user: current_user,
     }
-    result = GovukGraphqlSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
+    result = nil
+    sql_events = []
+    subscribe_to_sql_events(sql_events)
+    execution_time = Benchmark.measure do
+      result = GovukGraphqlSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
+    end
 
     schema_name = result.dig("data", "edition", "schema_name")
     locale = result.dig("data", "edition", "locale")
     set_prometheus_labels(schema_name:, locale:) if schema_name.present? || locale.present?
 
-    render json: result
+    render json: result.reverse_merge(execution_time_ms: format_benchmark(execution_time, sql_events))
   rescue StandardError => e
     raise e unless Rails.env.development?
 
@@ -54,5 +61,21 @@ private
     logger.error error.backtrace.join("\n")
 
     render json: { errors: [{ message: error.message, backtrace: error.backtrace }], data: {} }, status: 500
+  end
+
+  def subscribe_to_sql_events(sql_events)
+    ActiveSupport::Notifications.subscribe("sql.sequel") do |event|
+      sql_events << {
+        sql: event.payload[:sql]&.truncate(100),
+        duration: event.duration,
+      }
+    end
+  end
+
+  def format_benchmark(execution_time, sql_events)
+    execution_time.to_h
+               .except(:label)
+               .transform_values { (it * 1000).round(2) }
+               .merge(sql_events: sql_events.map { it.merge(duration: it[:duration].round(2)) })
   end
 end
